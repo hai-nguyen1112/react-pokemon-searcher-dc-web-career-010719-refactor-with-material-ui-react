3558e9bf3de47654f2b89c693b151349247c092e
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-06-30 11:37:26 -0400

    installed material-ui redux router-dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'semantic-ui-css/semantic.min.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import {BrowserRouter as Router} from 'react-router-dom'
+import store from './redux/store'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+ReactDOM.render(<Router><Provider store={store}><App /></Provider></Router>, document.getElementById('root'))
